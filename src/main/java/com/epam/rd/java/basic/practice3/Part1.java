504e1677710de0a6c719ac95ff27e41a316214cb
Oleksii Kirpilov <kirpilev.aleksey@gmail.com>
2021-07-08 19:28:38 +0300

    part1, convert1

--- src/main/java/com/epam/rd/java/basic/practice3/Part1.java
+++ src/main/java/com/epam/rd/java/basic/practice3/Part1.java
@@ -1,13 +1,39 @@
 package com.epam.rd.java.basic.practice3;
 
+import java.util.Arrays;
+import java.util.Iterator;
+
 public class Part1 {
 
+    private final static String fileName = "part1.txt";
+    private static String textData;
+
     public static void main(String[] args) {
-        
+        System.out.println("Part1 demo.");
+        System.out.println("input:");
+        textData = Util.getInput(fileName);
+        System.out.println(textData);
+        System.out.println();
+        System.out.println("convert1:");
+        System.out.println(convert1(textData));
+        System.out.println("convert2:");
+        System.out.println(convert2(textData));
+        System.out.println("convert3:");
+        System.out.println(convert3(textData));
+        System.out.println("convert4:");
+        System.out.println(convert4(textData));
     }
 
     public static String convert1(String input) {
-        return null;
+        Iterator<String> i = Arrays.stream(textData.split(System.lineSeparator())).iterator();
+        StringBuilder sb = new StringBuilder();
+        i.next();
+        while (i.hasNext()){
+            String []line = i.next().split(";");
+            sb.append(line[0]).append(": ").append(line[2]);
+            sb.append(System.lineSeparator());
+        }
+        return sb.toString();
     }
 
     public static String convert2(String input) {
