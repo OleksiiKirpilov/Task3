f5c10932001a1567bfaaa91dd2ae6feb5eb9d75c
Oleksii Kirpilov <kirpilev.aleksey@gmail.com>
2021-07-12 00:11:16 +0300

    part 6 test

--- src/main/java/com/epam/rd/java/basic/practice3/Part6.java
+++ src/main/java/com/epam/rd/java/basic/practice3/Part6.java
@@ -6,13 +6,11 @@ import java.util.regex.Pattern;
 public class Part6 {
 
     public static void main(String[] args) {
-        String textData = Util.getInput("part6.txt");
+        String input = Util.getInput("part6.txt");
         System.out.println("Part6 demo.");
-        System.out.println("input:");
-        System.out.println(textData);
+        System.out.println(input);
         System.out.println();
-        System.out.println("convert:");
-        System.out.println(convert(textData));
+        System.out.println(convert(input));
     }
 
     public static String convert(String input) {
@@ -29,8 +27,21 @@ public class Part6 {
     }
 
     private static boolean isRecurring(String word, String input) {
-        Pattern r = Pattern.compile("(" + word + ".*){2,}");
-        Matcher m = r.matcher(input);
-        return m.find();
+        int index = 0;
+        int count = 0;
+        while (count < 2) {
+            index = input.indexOf(word, index);
+            if (index == -1) {
+                return false;
+            }
+            count++;
+            index += word.length();
+        }
+        return true;
+
+
+//        Pattern r = Pattern.compile("(" + word + ".*){2,}");
+//        Matcher m = r.matcher(input);
+//        return m.find();
     }
 }
