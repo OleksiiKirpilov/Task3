384394b9cab24d6b952de41543ee763739ad48f3
Oleksii Kirpilov <kirpilev.aleksey@gmail.com>
2021-07-13 19:51:18 +0300

    small fixes

--- src/main/java/com/epam/rd/java/basic/practice3/Part5.java
+++ src/main/java/com/epam/rd/java/basic/practice3/Part5.java
@@ -1,8 +1,5 @@
 package com.epam.rd.java.basic.practice3;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 public class Part5 {
 
     public static void main(String[] args) {
--- src/main/java/com/epam/rd/java/basic/practice3/Util.java
+++ src/main/java/com/epam/rd/java/basic/practice3/Util.java
@@ -25,7 +25,7 @@ public class Util {
             scanner.close();
             return sb.toString().trim();
         } catch (IOException ex) {
-            getLogger().log(Level.ALL, ex.getMessage(), ex);
+            getLogger().log(Level.SEVERE, ex.getMessage(), ex);
         }
         return sb.toString();
     }
